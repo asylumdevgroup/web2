e21ae146aae8c823048416615d8c629538ccfb96
Tabitha Hanegan <Tabithahanegan@gmail.com>
2025-08-07 04:55:48 -0400

    huge derp

--- modpacks/views.py
+++ modpacks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.db import models
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
